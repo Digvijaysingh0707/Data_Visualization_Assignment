4226286cbedac88aeaf19617a10b41a1561a5c8f
Digvijay Singh <Digvijaysinghonly1@gmail.com>
2024-03-31 23:29:23 +0530

    flavanoid code modification

--- src/components/FlavanoidManagement.jsx
+++ src/components/FlavanoidManagement.jsx
@@ -1,71 +1,30 @@
 import { useEffect, useMemo, useState } from "react"
-import { groupData } from "../utils/helper"
+import { getMean, getMedian, getMode, groupData } from "../utils/helper"
 import { wineData } from "../constants/wineData";
 import Listing from "./FlavonoidsListing";
 
 const FlavanoidManagement = () => {
-    const [groupDataArray, setGroupDataArray] = useState({})
-
-    const getMean = useMemo(() => {
-        const flavanoids = groupDataArray;
-        const meanValues = {};
-        for (const alcohol in flavanoids) {
-            const values = flavanoids[alcohol];
-            const sum = values.reduce((acc, val) => acc + parseFloat(val), 0);
-            const mean = (sum / values.length).toFixed(3);
-            meanValues[alcohol] = mean;
-        }
-        return meanValues;
-    }, [groupDataArray])
-
-    const getMedian = useMemo(() => {
-        const flavanoids = groupDataArray;
-        const medianValues = {};
-        for (const alcohol in flavanoids) {
-            const values = flavanoids[alcohol];
-            const sortedValues = values.sort((a, b) => a - b);
-            const mid = Math.floor(sortedValues.length / 2);
-            const median =
-                sortedValues.length % 2 !== 0
-                    ? sortedValues[mid]
-                    : (sortedValues[mid - 1] + sortedValues[mid]) / 2;
-            medianValues[alcohol] = median;
-        }
-        return medianValues
-
-    }, [groupDataArray])
-
-    const getMode = useMemo(() => {
-        const flavanoids = groupDataArray;
-
-        const modeValues = {};
-        for (const alcohol in flavanoids) {
-            const values = flavanoids[alcohol];
-            const modeMap = {};
-            let maxCount = 0;
-            let mode;
-            values.forEach(val => {
-                modeMap[val] = (modeMap[val] || 0) + 1;
-                if (modeMap[val] > maxCount) {
-                    maxCount = modeMap[val];
-                    mode = val;
-                }
-            });
-            modeValues[alcohol] = mode;
-        }
-        return modeValues
-    }, [groupDataArray])
-
-
+    const [flavanoidMean, setFlavanoidMean] = useState()
+    const [flavanoidMedian, setFlavanoidMedian] = useState()
+    const [flavanoidMode, setFlavanoidMode] = useState()
 
     useEffect(() => {
         const groupedData = groupData(wineData, "flavanoids")
-        console.log(groupedData, 'DATA in group')
-        setGroupDataArray(groupedData)
+        if (groupedData && Object.values(groupedData)?.length > 0) {
+            const flavMean = getMean(groupedData)
+            const flavMode = getMode(groupedData)
+            const flavMedian = getMedian(groupedData)
+            setFlavanoidMean(flavMean)
+            setFlavanoidMode(flavMode)
+            setFlavanoidMedian(flavMedian)
+        }
     }, [])
 
     return (
-        <Listing mean={Object.values(getMean)} median={Object.values(getMedian)} mode={Object.values(getMode)} />
+        (
+            (flavanoidMean && Object.values(flavanoidMean)?.length > 0) &&
+            <Listing mean={Object.values(flavanoidMean)} median={Object.values(flavanoidMedian)} mode={Object.values(flavanoidMode)} />
+        )
     )
 
 }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -19,3 +19,48 @@ export const groupData = (data, type) => {
   });
   return groupedData;
 };
+
+export const getMean = (alcoholList) => {
+  const meanValues = {};
+  for (const alcohol in alcoholList) {
+    const values = alcoholList[alcohol];
+    const sum = values.reduce((acc, val) => acc + parseFloat(val), 0);
+    const mean = (sum / values.length).toFixed(3);
+    meanValues[alcohol] = mean;
+  }
+  return meanValues;
+};
+
+export const getMedian = (alcoholList) => {
+  const medianValues = {};
+  for (const alcohol in alcoholList) {
+    const values = alcoholList[alcohol];
+    const sortedValues = values.sort((a, b) => a - b);
+    const mid = Math.floor(sortedValues.length / 2);
+    const median =
+      sortedValues.length % 2 !== 0
+        ? sortedValues[mid]
+        : (sortedValues[mid - 1] + sortedValues[mid]) / 2;
+    medianValues[alcohol] = median;
+  }
+  return medianValues;
+};
+
+export const getMode = (alcoholList) => {
+  const modeValues = {};
+  for (const alcohol in alcoholList) {
+    const values = alcoholList[alcohol];
+    const modeMap = {};
+    let maxCount = 0;
+    let mode;
+    values.forEach((val) => {
+      modeMap[val] = (modeMap[val] || 0) + 1;
+      if (modeMap[val] > maxCount) {
+        maxCount = modeMap[val];
+        mode = val;
+      }
+    });
+    modeValues[alcohol] = mode;
+  }
+  return modeValues;
+};
